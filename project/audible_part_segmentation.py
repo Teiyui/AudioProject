5104632a3b24781e6c977d05580d56f6eabefd1b
Youwei Zheng <youwzhen@cisco.com>
2021-09-06 01:05:35 +0800

    【Second Commit】Add tips.

--- project/audible_part_segmentation.py
+++ project/audible_part_segmentation.py
@@ -3,9 +3,13 @@ import scipy.io.wavfile as wav
 import uuid
 import os
 
-filename = "../audio/speech_orig.wav"
+filename = "../audio/myrec.wav"
 (rate, sig) = wav.read(filename)
+
+# Machine Learning algorithm which provided by pyAudioAnalysis
 segments = audioSegmentation.silence_removal(sig, rate, 0.020, 0.020, smooth_window=0.5, weight=0.5)
+
+# Get true segmentations of recorded audio file
 true_segments = []
 for i in range(len(segments)):
     if segments[i][1] - segments[i][0] > 5:
@@ -13,6 +17,7 @@ for i in range(len(segments)):
         end = int(segments[i][1] * rate)
         true_segments.append([start, end])
 
+# Output the segmentation as .wav file
 filePath = str(uuid.uuid1())
 os.mkdir('../audio/' + filePath)
 for j in range(len(true_segments)):
